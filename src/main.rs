dfa6ea0cb0b22261d97a557a41754000a50b703d
Alberto <mail4alberto@gmail.com>
2016-05-31 08:06:27 -0400

    working ideal contexts

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 // #![deny(warnings)]
 extern crate hyper;
 extern crate regex;
+extern crate url;
 
 use std::collections::HashMap;
+use std::ffi::OsStr;
+use std::path::{PathBuf, Path};
+
+use url::percent_encoding::percent_decode;
 
 use regex::{Regex, RegexSet};
 use regex::{Captures};
@@ -19,6 +24,10 @@ static PHRASE: &'static [u8] = b"Hello World!";
 fn main() {
 
     // TODO: clap-rs
+
+    let global_context = GlobalContext {
+        assets_root_path: Path::new("assets/")
+    };
 
     /* router setup */
 
@@ -44,22 +53,28 @@ fn main() {
 
     let _guard = server.handle(move |request: Request, response: Response| {
 
+        let uri = format!("{}", request.uri);
 
         let mut context = Context {
-            uri: &format!("{}", request.uri),
+            global_context: &global_context,
+            request: request,
+            response: response,
+            uri: &uri,
             captures: None
         };
 
         // middleware/router
 
-        let handler = match router.handle(&mut context, &request) {
+        let handler = match router.handle(&mut context) {
             Some(handler) => {
                 handler
             },
             None => route_not_found
         };
 
-        handler(context, request, response);
+        handler(context);
+
+        // let context = context;
 
         // response.send(PHRASE).unwrap();
     });
@@ -67,20 +82,43 @@ fn main() {
     println!("Listening on http://127.0.0.1:3000");
 }
 
+/* contexts */
+
+struct GlobalContext<'a> {
+    assets_root_path: &'a Path
+}
+
+// struct Context<'a, 'b, 'c, 'd: 'c> {
+struct Context<
+    'global,
+
+    'regex,
+
+    // hyper lifetimes
+    'a, 'k: 'a, 'b
+    > {
+    global_context: &'global GlobalContext<'global>,
+
+    uri: &'regex str,
+    captures: Option<Captures<'regex>>,
+
+    request: Request<'a, 'k>,
+    response: Response<'b>
+
+
+}
+
+
 /* router */
 // Source: https://github.com/gsquire/reroute
 
-struct Context<'c> {
-
-    uri: &'c str,
-    captures: Option<Captures<'c>>
-}
-
-type RouterFn = fn(Context, Request, Response);
+type RouterFn = fn(Context);
 
 #[derive(PartialEq, Eq, Hash)]
 struct RouteInfo {
     // route: String,
+
+    // fast path
     route_map_idx: usize,
     verb: Method
 }
@@ -127,7 +165,7 @@ impl Router {
     // function passing the hyper Request and Response structures.
     //
     // Returning None implies no route handler found.
-    fn handle<'c>(&'c self, context: &mut Context<'c>, request: &Request) -> Option<RouterFn> {
+    fn handle<'a, 'b, 'c, 'd, 'e>(&'a self, context: &mut Context<'a, 'b, 'c, 'd, 'e>) -> Option<RouterFn> {
 
         // TODO: remove
         // let uri = format!("{}", request.uri);
@@ -141,7 +179,7 @@ impl Router {
                 // let route = &self.route_list[i];
                 let route_info = RouteInfo{
                     route_map_idx: matched_idx,
-                    verb: request.method.clone()
+                    verb: context.request.method.clone()
                 };
 
                 let handler = self.route_map.get(&route_info);
@@ -221,19 +259,49 @@ impl Router {
 
 /* routes */
 
-fn route_not_found(ctx: Context, request: Request, mut response: Response) {
-    let message = format!("No route handler found for {}", request.uri);
-    *response.status_mut() = StatusCode::NotFound;
-    response.send(message.as_bytes()).unwrap();
+fn route_not_found(mut context: Context) {
+
+    // let mut context = context;
+
+    let message = format!("No route handler found for {}", context.request.uri);
+    *context.response.status_mut() = StatusCode::NotFound;
+
+    context.response.send(message.as_bytes()).unwrap();
 }
 
-fn route_static_assets(ctx: Context, _: Request, mut response: Response) {
+fn route_static_assets(context: Context) {
 
-    let path = ctx.captures.unwrap().name("path").unwrap();
+    let req_path_raw = context.captures.unwrap().name("path").unwrap();
 
-    let message = format!("Path {}", path);
+    // let req_path = Path::new(&req_path);
+    let decoded_req_path = Path::new(&req_path_raw).iter().map(decode_percents);
 
-    response.send(message.as_bytes()).unwrap();
+    let mut req_path = context.global_context.assets_root_path.to_path_buf();
+    req_path.extend(decoded_req_path);
+    let req_path = req_path;
+
+    // TODO: fetch file
+
+    // TODO: stream file to output
+
+    // https://github.com/iron/iron/blob/2fd6e079592ec340d8c75200bd54fde91ce34d51/src/modifiers.rs#L116-L154
+
+    let message = format!("Path {:?}", req_path);
+
+    context.response.send(message.as_bytes()).unwrap();
 
     // response.send(PHRASE).unwrap();
+}
+
+/* helpers */
+
+#[inline]
+fn decode_percents(string: &OsStr) -> String {
+
+    let string = format!("{}", string.to_string_lossy());
+
+    format!("{}", percent_decode(string.as_bytes()).decode_utf8_lossy())
+
+    // String::from_utf8(.if_any().unwrap()).unwrap()
+    // OsStr::new(&token)
 }
