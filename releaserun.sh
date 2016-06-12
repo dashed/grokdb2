cab4ec4b7fcb1909f06802f5a4733388f60be2b2
Alberto <mail4alberto@gmail.com>
2016-06-12 07:49:48 -0400

    asdasd

---
+++ releaserun.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+cargo build --release
+# RUST_BACKTRACE=1 target/debug/gtdtxt reference-todo.gtd
+# target/debug/gtdtxt empty.gtd
+# RUST_BACKTRACE=1 target/debug/gtdtxt test2.gtd
+# RUST_BACKTRACE=1 target/debug/gtdtxt test2.gtd -x -k=bar
+# -x --due-within=2day --show-flagged --show-with-project=foo
+# --show-overdue --show-incomplete --show-deferred --show-done
+
+
+RUST_BACKTRACE=1 target/release/grokdb2
+# RUST_BACKTRACE=1 target/debug/gtdtxt test3.gtd
