014efb99a329c705a7c1016d6eed075432580ea3
Alberto <mail4alberto@gmail.com>
2016-06-24 17:10:23 -0400

    custom serialization for APIStatus

--- src/errors.rs
+++ src/errors.rs
@@ -1,20 +1,43 @@
+/* 3rd-party imports */
+
+use serde::{Serialize, Serializer};
+
+////////////////////////////////////////////////////////////////////////////////
+
 // src: https://github.com/WhiteHouse/api-standards#error-handling
 
 // 200 - OK
 // 400 - Bad Request
 // 500 - Internal Server Error
 
-#[derive(Debug, PartialEq, Serialize)]
+#[derive(Debug, PartialEq)]
 pub enum APIStatus {
 
-    #[serde(rename = "Reques success")]
+    // #[serde(rename = "Reques success")]
     Ok, // 200
 
-    #[serde(rename = "Failure due to client-side problem")]
+    // #[serde(rename = "Failure due to client-side problem")]
     BadRequest, // 400
 
-    #[serde(rename = "Failure due to server-side problem")]
+    // #[serde(rename = "Failure due to server-side problem")]
     ServerError // 500
+}
+
+// NOTE: #[derive(Serialize)] is not used. Need custom serialization.
+impl Serialize for APIStatus {
+
+    // serialize APIStatus to human friendly message
+    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+        where S: Serializer {
+
+        let human_message = match self {
+            &APIStatus::Ok => "Request success.",
+            &APIStatus::BadRequest => "Failure due to client-side problem.",
+            &APIStatus::ServerError => "Failure due to server-side problem.",
+        };
+
+        return serializer.serialize_str(human_message);
+    }
 }
 
 #[derive(Debug, PartialEq, Serialize)]
