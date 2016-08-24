bb197be93f9e96974e7e0316ff6c48c3da20c7eb
Alberto <mail4alberto@gmail.com>
2016-08-24 11:38:18 -0400

    fix

--- client_src/src/components/deck_description.js
+++ client_src/src/components/deck_description.js
@@ -49,6 +49,23 @@ const initialState = {
 
 };
 
+/* exports */
+
+const merge = require('lodash/merge');
+
+const formReducer = (state, action) => {
+
+    // NOTE: We're not using combineReducers from redux as redux-form expects.
+    //       Defer any un-captured action to redux-form.
+
+    const newForm = reduxformReducer(state.form, action);
+    const newState = merge({}, state);
+    newState.form = newForm;
+
+    return newState;
+};
+
+
 module.exports = function(preRenderState) {
 
     if(preRenderState) {
