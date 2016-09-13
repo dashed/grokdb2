d610e1cf115b3b0989a3bb70d4953afb82162806
Alberto <mail4alberto@gmail.com>
2016-09-12 23:46:33 -0400

    generate timezones

---
+++ src/timezones/Europe/mod.rs
@@ -0,0 +1,180 @@
+mod Amsterdam;
+pub use self::Amsterdam::ZONE as Amsterdam;
+
+mod Andorra;
+pub use self::Andorra::ZONE as Andorra;
+
+mod Astrakhan;
+pub use self::Astrakhan::ZONE as Astrakhan;
+
+mod Athens;
+pub use self::Athens::ZONE as Athens;
+
+mod Belgrade;
+pub use self::Belgrade::ZONE as Belgrade;
+
+mod Berlin;
+pub use self::Berlin::ZONE as Berlin;
+
+mod Bratislava;
+pub use self::Bratislava::ZONE as Bratislava;
+
+mod Brussels;
+pub use self::Brussels::ZONE as Brussels;
+
+mod Bucharest;
+pub use self::Bucharest::ZONE as Bucharest;
+
+mod Budapest;
+pub use self::Budapest::ZONE as Budapest;
+
+mod Busingen;
+pub use self::Busingen::ZONE as Busingen;
+
+mod Chisinau;
+pub use self::Chisinau::ZONE as Chisinau;
+
+mod Copenhagen;
+pub use self::Copenhagen::ZONE as Copenhagen;
+
+mod Dublin;
+pub use self::Dublin::ZONE as Dublin;
+
+mod Gibraltar;
+pub use self::Gibraltar::ZONE as Gibraltar;
+
+mod Guernsey;
+pub use self::Guernsey::ZONE as Guernsey;
+
+mod Helsinki;
+pub use self::Helsinki::ZONE as Helsinki;
+
+mod Isle_of_Man;
+pub use self::Isle_of_Man::ZONE as Isle_of_Man;
+
+mod Istanbul;
+pub use self::Istanbul::ZONE as Istanbul;
+
+mod Jersey;
+pub use self::Jersey::ZONE as Jersey;
+
+mod Kaliningrad;
+pub use self::Kaliningrad::ZONE as Kaliningrad;
+
+mod Kiev;
+pub use self::Kiev::ZONE as Kiev;
+
+mod Kirov;
+pub use self::Kirov::ZONE as Kirov;
+
+mod Lisbon;
+pub use self::Lisbon::ZONE as Lisbon;
+
+mod Ljubljana;
+pub use self::Ljubljana::ZONE as Ljubljana;
+
+mod London;
+pub use self::London::ZONE as London;
+
+mod Luxembourg;
+pub use self::Luxembourg::ZONE as Luxembourg;
+
+mod Madrid;
+pub use self::Madrid::ZONE as Madrid;
+
+mod Malta;
+pub use self::Malta::ZONE as Malta;
+
+mod Mariehamn;
+pub use self::Mariehamn::ZONE as Mariehamn;
+
+mod Minsk;
+pub use self::Minsk::ZONE as Minsk;
+
+mod Monaco;
+pub use self::Monaco::ZONE as Monaco;
+
+mod Moscow;
+pub use self::Moscow::ZONE as Moscow;
+
+mod Nicosia;
+pub use self::Nicosia::ZONE as Nicosia;
+
+mod Oslo;
+pub use self::Oslo::ZONE as Oslo;
+
+mod Paris;
+pub use self::Paris::ZONE as Paris;
+
+mod Podgorica;
+pub use self::Podgorica::ZONE as Podgorica;
+
+mod Prague;
+pub use self::Prague::ZONE as Prague;
+
+mod Riga;
+pub use self::Riga::ZONE as Riga;
+
+mod Rome;
+pub use self::Rome::ZONE as Rome;
+
+mod Samara;
+pub use self::Samara::ZONE as Samara;
+
+mod San_Marino;
+pub use self::San_Marino::ZONE as San_Marino;
+
+mod Sarajevo;
+pub use self::Sarajevo::ZONE as Sarajevo;
+
+mod Simferopol;
+pub use self::Simferopol::ZONE as Simferopol;
+
+mod Skopje;
+pub use self::Skopje::ZONE as Skopje;
+
+mod Sofia;
+pub use self::Sofia::ZONE as Sofia;
+
+mod Stockholm;
+pub use self::Stockholm::ZONE as Stockholm;
+
+mod Tallinn;
+pub use self::Tallinn::ZONE as Tallinn;
+
+mod Tirane;
+pub use self::Tirane::ZONE as Tirane;
+
+mod Ulyanovsk;
+pub use self::Ulyanovsk::ZONE as Ulyanovsk;
+
+mod Uzhgorod;
+pub use self::Uzhgorod::ZONE as Uzhgorod;
+
+mod Vaduz;
+pub use self::Vaduz::ZONE as Vaduz;
+
+mod Vatican;
+pub use self::Vatican::ZONE as Vatican;
+
+mod Vienna;
+pub use self::Vienna::ZONE as Vienna;
+
+mod Vilnius;
+pub use self::Vilnius::ZONE as Vilnius;
+
+mod Volgograd;
+pub use self::Volgograd::ZONE as Volgograd;
+
+mod Warsaw;
+pub use self::Warsaw::ZONE as Warsaw;
+
+mod Zagreb;
+pub use self::Zagreb::ZONE as Zagreb;
+
+mod Zaporozhye;
+pub use self::Zaporozhye::ZONE as Zaporozhye;
+
+mod Zurich;
+pub use self::Zurich::ZONE as Zurich;
+
---
+++ src/timezones/Indian/mod.rs
@@ -0,0 +1,33 @@
+mod Antananarivo;
+pub use self::Antananarivo::ZONE as Antananarivo;
+
+mod Chagos;
+pub use self::Chagos::ZONE as Chagos;
+
+mod Christmas;
+pub use self::Christmas::ZONE as Christmas;
+
+mod Cocos;
+pub use self::Cocos::ZONE as Cocos;
+
+mod Comoro;
+pub use self::Comoro::ZONE as Comoro;
+
+mod Kerguelen;
+pub use self::Kerguelen::ZONE as Kerguelen;
+
+mod Mahe;
+pub use self::Mahe::ZONE as Mahe;
+
+mod Maldives;
+pub use self::Maldives::ZONE as Maldives;
+
+mod Mauritius;
+pub use self::Mauritius::ZONE as Mauritius;
+
+mod Mayotte;
+pub use self::Mayotte::ZONE as Mayotte;
+
+mod Reunion;
+pub use self::Reunion::ZONE as Reunion;
+
